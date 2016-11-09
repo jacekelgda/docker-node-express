ef454791b04cf739b9a9f75f8bac059c98b675de
jacekelgda <jacekelgda@gmail.com>
2016-11-09 12:12:31 +0100

    cleanup mr.moody

--- app/app.js
+++ app/app.js
@@ -1,10 +1,176 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const Slack = require('slack-node');
+const app = express();
+const config = require('config');
+const https = require('https');
+const postMessageMethod = 'chat.postMessage';
+const listUsersMethod = 'users.list';
+const attachments = JSON.stringify(
+  [
+    {
+      "fallback": "How was your week ?",
+      "pretext": "How was your week ?",
+      "callback_id": "mrmoody_v1",
+      "attachment_type": "default",
+      "color": "good",
+      "actions": [
+          {
+              "name": "smile",
+              "text": ":smile:",
+              "type": "button",
+              "value": "smile"
+          },
+          {
+              "name": "neutral_face",
+              "text": ":neutral_face:",
+              "type": "button",
+              "value": "neutral_face"
+          },
+          {
+              "name": "disappointed",
+              "text": ":disappointed:",
+              "type": "button",
+              "value": "disappointed"
+          }
+      ]
+    }
+  ]
+);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+router.use(function timeLog (req, res, next) {
+  next();
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+router.get('/', function(req, res) {
+  res.send('API is running ...');
+});
+
+router.post('/notify', function (req, res) {
+		if (req.body.payload) {
+			var payload = JSON.parse(req.body.payload);
+
+			// extract to config
+			if (payload.token == 'LdFvO6j7WmKbwz2uSMQaj09N') {
+				var vote = payload.actions[0].value;
+				var username = payload.user.name;
+				switch (vote) {
+				  case 'smile':
+				    console.log(username, 'smile');
+						var slack_api_token = process.env.api_token;
+						if (!process.env.api_token) {
+							slack_api_token = config.get('api.oauth.api_token');
+						}
+						url = 'https://slack.com/api/' + postMessageMethod +
+							'?token=' + slack_api_token +
+							'&username=Mr. Moody' +
+							'&as_user=false' +
+							'&icon_url=https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2016-11-04/100929399430_30f602e36ebfbc81756b_48.jpg' +
+							'&channel=mrmoody-happy' +
+							'&text=:smile: ' + username;
+
+						https.get(url, (res) => {
+						}).on('error', (e) => {
+							console.error(e);
+						});
+				    break;
+					case 'neutral_face':
+						console.log(username, 'neutral_face');
+						var slack_api_token = process.env.api_token;
+						if (!process.env.api_token) {
+							slack_api_token = config.get('api.oauth.api_token');
+						}
+						url = 'https://slack.com/api/' + postMessageMethod +
+							'?token=' + slack_api_token +
+							'&username=Mr. Moody' +
+							'&as_user=false' +
+							'&icon_url=https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2016-11-04/100929399430_30f602e36ebfbc81756b_48.jpg' +
+							'&channel=mrmoody-neutral' +
+							'&text=:neutral_face: ' + username;
+
+						https.get(url, (res) => {
+						}).on('error', (e) => {
+							console.error(e);
+						});
+				    break;
+					case 'disappointed':
+						console.log(username, 'disappointed');
+						var slack_api_token = process.env.api_token;
+						if (!process.env.api_token) {
+							slack_api_token = config.get('api.oauth.api_token');
+						}
+						url = 'https://slack.com/api/' + postMessageMethod +
+							'?token=' + slack_api_token +
+							'&username=Mr. Moody' +
+							'&as_user=false' +
+							'&icon_url=https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2016-11-04/100929399430_30f602e36ebfbc81756b_48.jpg' +
+							'&channel=mrmoody-sad' +
+							'&text=:disappointed: ' + username;
+
+						https.get(url, (res) => {
+						}).on('error', (e) => {
+							console.error(e);
+						});
+				    break;
+				  default:
+				    console.log('wrong answer', vote);
+				    break;
+				}
+			} else {
+				console.log('token is not valid');
+			}
+
+			// reponse
+			var message = {
+		    "text": "Thank you for your answer. Have a great weekend!",
+			};
+			res.json(message);
+
+		} else {
+			res.send('Invalid param');
+		}
+
 })
+
+router.post('/send-all', function (req, res) {
+	if (req.body.user_name == 'jacek.lawniczak') {
+		var slack_api_token = process.env.api_token;
+		if (!process.env.api_token) {
+			slack_api_token = config.get('api.oauth.api_token');
+		}
+
+		// slack = new Slack(slack_api_token);
+		// slack.api('users.list', {
+		// }, function(err, response){
+    //
+		//   for(var index in response.members){
+		//     if(!response.members[index].is_bot && !response.members[index].deleted) {
+    //       slackUsername = response.members[index].name;
+		// 				url = 'https://slack.com/api/' + postMessageMethod +
+		// 		      '?token=' + slack_api_token +
+		// 					'&username=Mr. Moody' +
+		// 					'&as_user=false' +
+		// 					'&icon_url=https://s3-us-west-2.amazonaws.com/slack-files2/avatars/2016-11-04/100929399430_30f602e36ebfbc81756b_48.jpg' +
+		// 		      '&channel=@' + slackUsername +
+		// 		      '&attachments=' + attachments;
+    //
+		// 		    https.get(url, (res) => {
+		// 		    }).on('error', (e) => {
+		// 		      console.error(e);
+		// 		    });
+		//     }
+		//   }
+		// });
+
+		res.send('"How was your week?" - notifications sent to private channels. Thanks!');
+	}
+})
+
+app.use('/api', router);
+app.listen(port);
